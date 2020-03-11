0421dae4f00b58026666a1109ec0c41aca7a0a07
Utilisateur <user@debian.arcahe.ovh>
2020-03-11 10:11:52 +0100

    init commit et creation squelette classe

---
+++ src/annuaire/Application.java
@@ -0,0 +1,13 @@
+package annuaire;
+
+enum Application {
+	ENVIRONNEMENT;
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/annuaire/Personnel.java
@@ -0,0 +1,15 @@
+package annuaire;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class Personnel {
+
+	private String nom;
+	private String prenom;
+	private String fonction;
+	private java.time.LocalDate datenaissance;
+	private Collection<PhoneNumber> telephone;
+	
+	
+}

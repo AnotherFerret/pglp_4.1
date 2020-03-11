9d70f94fe9b2ff1c5410609555cd4a6d3134ed54
Utilisateur <user@debian.arcahe.ovh>
2020-03-11 11:12:39 +0100

    ecriture constructeurs builder

--- src/annuaire/Application.java
+++ src/annuaire/Application.java
@@ -1,13 +1,32 @@
 package annuaire;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+
 enum Application {
 	ENVIRONNEMENT;
 	
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+		LocalDate d = LocalDate.of(1975, 03, 19);
+		HashSet<PhoneNumber> t = new HashSet<PhoneNumber>();
+		t.add(new PhoneNumber("0123456789"));
+		t.add(new PhoneNumber("9876543210"));
+		
+		
+		Personnel p1 = new Personnel
+					.Builder("name", "name2")
+					.build();
+		
+		Personnel p2 = new Personnel
+						.Builder("name3", "name4")
+						.fonction("random task")
+						.datenaissance(d)
+						.telephone(t)
+						.build();
+	}					
+			
 
 }
--- src/annuaire/PhoneNumber.java
+++ src/annuaire/PhoneNumber.java
@@ -2,4 +2,18 @@ package annuaire;
 
 public class PhoneNumber {
 
+	String phone; 
+	
+	public PhoneNumber(String phone)
+	{
+		if(phone.length() < 10)
+		{
+			//exception à gérer
+			this.phone = "0000000000";
+		}
+		else
+		{
+			this.phone = phone;
+		}
+	}
 }
